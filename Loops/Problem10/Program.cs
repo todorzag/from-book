320a67ad5e1ada10137fc57c9a6e0abbf0453ee5
Todor Zagorov <todorflorida20@gmail.com>
2022-08-03 10:52:28 +0300

    Added Loops

---
+++ Loops/Problem10/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Problem10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            int counter = 1;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = row + col + 1;
+                }
+            }
+
+            PrintMatrix(matrix);
+        }
+
+        public static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
