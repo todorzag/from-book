fd33115e67b1f1ceed694ae03b374b3e67119b87
Todor Zagorov <todorflorida20@gmail.com>
2022-08-05 11:39:58 +0300

    Added Objects Tasks

---
+++ Objects/Problem11/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Problem11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter product:");
+            string product = Console.ReadLine();
+
+            AdvertismentMaker advertismentMaker = new AdvertismentMaker(product);
+
+            Console.WriteLine(advertismentMaker.MakeAdvert());
+            Console.WriteLine(advertismentMaker.MakeAdvert());
+        }
+    }
+}
