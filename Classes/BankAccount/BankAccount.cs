152513e7687c4b7d778e1e8f887e3d0468eb8e4e
Todor Zagorov <todorflorida20@gmail.com>
2022-08-05 17:05:09 +0300

    Added Bank Account Task (From Microsoft Documentation)

---
+++ Classes/BankAccount/BankAccount.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    internal class BankAccount
+    {
+        private static int accountNumberSeed = 1;
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance 
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var transaction in allTransactions)
+                {
+                    balance += transaction.Amount;
+                }
+
+                return balance;
+            } 
+        }
+
+        private void GenerateRandomAccountSeed()
+        {
+            Random random = new Random();
+            accountNumberSeed =  random.Next(0, 1000000);
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+
+            Transaction deposit = new Transaction(amount, date, "deposit", note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), 
+                    "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+
+            Transaction withdrawal = new Transaction(-amount, date, "withdrawal", note);
+            allTransactions.Add(withdrawal);
+        }
+
+        public string GetAccountHistory()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var transaction in allTransactions)
+            {
+
+                sb.Append($"A {transaction.Type} was made on: {transaction.Date}\n");
+
+                sb.Append($"For: {
+                    (transaction.Amount < 0 
+                    ? transaction.Amount * -1 
+                    : transaction.Amount)
+                    }\n");
+
+                sb.Append($"Note: {transaction.Note}\n");
+            }
+
+            return sb.ToString();
+        }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+
+            GenerateRandomAccountSeed();
+            Number = accountNumberSeed.ToString();
+        }
+    }
+}
---
+++ Classes/BankAccount/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter bank account name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Please enter initial balance:");
+            decimal initialBalance = ValidateAmount();
+
+            BankAccount account = new BankAccount(name, initialBalance);
+
+            MakeTransaction(account);
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+
+
+        private static decimal ValidateAmount()
+        {
+            decimal amount = int.MinValue;
+            try
+            {
+                amount = decimal.Parse(Console.ReadLine());
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Please enter a numeric value!");
+                ValidateAmount();
+            }
+
+            return amount;
+        }
+
+        private static void MakeTransaction(BankAccount account)
+        {
+            Console.WriteLine("What kind of transaction would you like to make?");
+            string transactionType = Console.ReadLine();
+            ValidateTransactionType(transactionType);
+
+            Console.WriteLine("For what amount?");
+            decimal amount = ValidateAmount();
+            string note = AddNote();
+
+            switch (transactionType)
+            {
+                case "deposit":
+                    account.MakeDeposit(amount, DateTime.Now, note);
+                    break;
+
+                case "withdrawal":
+                    account.MakeWithdrawal(amount, DateTime.Now, note);
+                    break;
+            }
+
+            Console.WriteLine("Transaction Completed!");
+            Console.WriteLine("Make another transaction?");
+            string answer = Console.ReadLine();
+
+            switch (answer)
+            {
+                case "yes":
+                    MakeTransaction(account);
+                    break;
+            }
+        }
+
+        private static string AddNote()
+        {
+            Console.WriteLine("Please add a note with your transaction.");
+            string note = Console.ReadLine();
+
+            return note;
+        }
+
+        private static string ValidateTransactionType(string transactionType)
+        {
+            if (transactionType != "deposit" && transactionType != "withdrawal")
+            {
+                Console.WriteLine("Please enter valid transaction type!");
+
+                transactionType = Console.ReadLine();
+                ValidateTransactionType(transactionType);
+            }
+
+            return transactionType;
+        }
+    }
+}
