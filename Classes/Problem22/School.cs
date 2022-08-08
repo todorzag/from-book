c8949589b6bc2566441d8be070837657d422b72f
Todor Zagorov <todorflorida20@gmail.com>
2022-08-08 12:36:31 +0300

    Added Problem22

---
+++ Classes/Problem22/School.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem22
+{
+    internal class School
+    {
+        public List<SchoolClass> SchoolClasses { get; }
+        public List<Student> Students { get; }
+
+        public School (List<SchoolClass> schoolClasses, List<Student> students)
+        {
+            SchoolClasses = schoolClasses;
+            Students = students;
+        }
+
+        public void PrintSchoolInfo()
+        {
+            foreach (SchoolClass schoolClass in SchoolClasses)
+            {
+                Console.WriteLine(schoolClass.ToString());
+            }
+        }
+    }
+}
