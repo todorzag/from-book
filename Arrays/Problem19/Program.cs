91160202b44dba1a7623858fd2c3a36f94f2e386
Todor Zagorov <todorflorida20@gmail.com>
2022-08-09 16:59:20 +0300

    Fixed Issues

--- Arrays/Problem19/Program.cs
+++ Arrays/Problem19/Program.cs
@@ -9,11 +9,14 @@
                Write a program, which finds all prime numbers in the range
                [1 … 1,000,000].
              */
+
             const int max = 100000;
 
             bool[] primes = new bool[max];
             primes = Array.ConvertAll(primes, prime => prime = true);
 
+
+            // Using "The sieve of Erathostenes"
             for (int i = 2; i < Math.Sqrt(max); i++)
             {
                 if (primes[i])
@@ -25,22 +28,23 @@
                 }
             }
 
-            PrintResult(primes);
+            // PrintResult(primes);
         }
 
+        // Added just for testing
         public static void PrintResult(bool[] primes)
         {
-            int count = 0;
+            List<int> result = new List<int>();
 
             for (int i = 2; i < primes.Length; i++)
             {
                 if (primes[i])
                 {
-                    count++;
+                    result.Add(i);
                 }
             }
 
-            Console.WriteLine($"There are {count} primes from 1 to 1,000,000");
+            Console.WriteLine(String.Join(", ", result));
         }
     }
 }
