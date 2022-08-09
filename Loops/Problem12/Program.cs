bf20998542670f98536e6588b42547f598a18dbd
Todor Zagorov <todorflorida20@gmail.com>
2022-08-09 15:06:46 +0300

    Added Loops

---
+++ Loops/Problem12/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Problem12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quotient = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>();
+
+            while (quotient != 0)
+            {
+                int remainder = quotient % 2;
+                quotient /= 2;
+                stack.Push(remainder);
+            }
+
+            PrintBinary(stack);
+        }
+
+        public static void PrintBinary(Stack<int> stack)
+        {
+            foreach (int i in stack)
+            {
+                Console.Write(i);
+            }
+        }
+
+    }
+}
---
+++ Loops/Problem16/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Problem16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+
+            Random random = new Random();
+
+            for (int i = 1; i <= n; i++)
+            {
+                int index = GetRandomIndex(array);
+                array[index] = i;
+            }
+
+            PrintArray(array);
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        public static int GetRandomIndex(int[] array)
+        {
+            Random random = new Random();
+
+            int index = random.Next(0, array.Length);
+
+            if (array[index] != 0)
+            {
+                index = GetRandomIndex(array);
+            }
+
+            return index;
+        }
+    }
+}
