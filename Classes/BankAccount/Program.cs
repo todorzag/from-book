4a74788b56444e60370e6d21dbc738d21097b870
Todor Zagorov <todorflorida20@gmail.com>
2022-08-09 12:47:51 +0300

    Added Classes And Bank Account

---
+++ Classes/BankAccount/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter bank account name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Please enter initial balance:");
+            decimal initialBalance = ValidateAmount();
+
+            BankAccount account = new BankAccount(name, initialBalance);
+
+            TransactionHandler(account);
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+
+
+        private static decimal ValidateAmount()
+        {
+            decimal amount = int.MinValue;
+            try
+            {
+                amount = decimal.Parse(Console.ReadLine());
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Please enter a numeric value!");
+                ValidateAmount();
+            }
+
+            return amount;
+        }
+
+        private static void TransactionHandler(BankAccount account)
+        {
+            Console.WriteLine("What kind of transaction would you like to make?");
+            Console.WriteLine("Deposit or Withdrawal?");
+
+            string transactionType = Console.ReadLine();
+            ValidateTransactionType(transactionType);
+
+            Console.WriteLine("For what amount?");
+            decimal amount = ValidateAmount();
+            string note = AddNote();
+
+            switch (transactionType)
+            {
+                case "Deposit":
+                    account.MakeDeposit(amount, note);
+                    break;
+
+                case "Withdrawal":
+                    account.MakeWithdrawal(amount, note);
+                    break;
+            }
+
+            Console.WriteLine("Transaction Completed!");
+            Console.WriteLine("Make another transaction?");
+            string answer = Console.ReadLine();
+
+            switch (answer)
+            {
+                case "Yes":
+                    TransactionHandler(account);
+                    break;
+            }
+        }
+
+        private static string AddNote()
+        {
+            Console.WriteLine("Please add a note with your transaction.");
+            string note = Console.ReadLine();
+
+            return note;
+        }
+
+        private static string ValidateTransactionType(string transactionType)
+        {
+            if (transactionType != "Deposit" && transactionType != "Withdrawal")
+            {
+                Console.WriteLine("Please enter valid transaction type!");
+                Console.WriteLine("Deposit or Withdrawal");
+
+                transactionType = Console.ReadLine();
+                ValidateTransactionType(transactionType);
+            }
+
+            return transactionType;
+        }
+    }
+}
---
+++ Classes/Problem22/SchoolClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem22
+{
+    internal class SchoolClass
+    {
+        public List<Teacher> Teachers { get; set; }
+        public string Name { get; set; }
+
+        public SchoolClass(string name)
+        {
+            Name = name;
+            Teachers = new List<Teacher>();
+        }
+
+        public SchoolClass (string name, List<Teacher> teachers)
+        {
+            Name = name;
+            Teachers = teachers; 
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append($"Teachers in {Name} class:\n");
+
+            foreach (Teacher teacher in Teachers)
+            {
+                sb.Append($" {teacher.ToString()}\n");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
