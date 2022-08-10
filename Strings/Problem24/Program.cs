348c6c9661e965bd1a37d3ad0914c1b2c8ec1b1a
Todor Zagorov <todorflorida20@gmail.com>
2022-08-10 15:09:36 +0300

    Fixed "if" logic in problem24

--- Strings/Problem24/Program.cs
+++ Strings/Problem24/Program.cs
@@ -16,9 +16,9 @@ namespace Problem24
 
             StringBuilder stringBuilder = new StringBuilder();
 
-            for (int i = 1; i < input.Length; i++)
+            for (int i = 0; i < input.Length; i++)
             {
-                if (input[i - 1] != input[i] || i - 1 == 0)
+                if (i == 0 || input[i - 1] != input[i])
                 {
                     stringBuilder.Append(input[i]);
                 } 
